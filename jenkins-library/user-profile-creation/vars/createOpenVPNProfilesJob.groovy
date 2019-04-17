d541289e2736527af733c77d6bd1b2264b550402
IliaUkhov <ilia.ukhov@gmail.com>
2019-04-17 14:42:26 +0000

    Updated profile creation pipeline

--- jenkins-library/user-profile-creation/vars/createOpenVPNProfilesJob.groovy
+++ jenkins-library/user-profile-creation/vars/createOpenVPNProfilesJob.groovy
@@ -2,15 +2,17 @@
 
 def call() {
     node {
-	stage('Check List') {
-            def users
+        def users
+        sh 'if [ ! -d sealed-configs ]; then mkdir sealed-configs; fi'
+        stage('Check List') {
             dir('digitallyinspired-todo-deployment/jenkins-users') {
                 users = readYaml(file: 'users.yml').users
-                sh 'if [ ! -d sealed-configs ]; then mkdir configs; fi'
             }
-            dir('digitallyinspired-todo-deployment/jenkins-users/configs') {
+        }
+        stage('Create OVPN profiles') {
+            dir('digitallyinspired-todo-deployment/jenkins-users/sealed-configs') {
                 for (user in users) {
-                    if (fileExists("${it}.ovpn")) continue
+                    if (fileExists("${it}-ovpn.yaml.sealed")) continue
                     def passphrase = sh returnStdout: true, script: 'cat /dev/urandom | base64 | head -c 10'
                     def built = build job: 'CreateOpenVPNProfile', parameters: [
                         [$class: 'StringParameterValue', name: 'OVPN_USER_NAME', value: user.name],
@@ -25,31 +27,58 @@ def call() {
                     )
                 }
                 sh 'mv client-configs/files/newest-user/* .'
-                stash(includes: '*.ovpn', name: 'new_profiles')
+                stash(includes: '*.ovpn', name: 'new_ovpn_profiles')
             }
-	}
-	stage('Make secrets') {
-            dir('digitallyinspired-todo-deployment/jenkins-users') {
-                unstash('new_profiles')
+        }
+        stage('Create K8S profiles') {
+            dir('digitallyinspired-todo-deployment/jenkins-users/sealed-configs') {
+                for (user in users) {
+                    if (fileExists("${it}-k8s.yaml.sealed")) continue
+                    def built = build job: 'CreateK8SNamespaceProfile', parameters: [
+                        [$class: 'StringParameterValue', name: 'USER_NAME', value: user.name],
+                        [$class: 'StringParameterValue', name: 'NAMESPACE', value: 'todo-dev']
+                    ]
+                    copyArtifacts(
+                        projectName: 'CreateK8SNamespaceProfile',
+                        selector: specific("${built.number}"),
+                        target: '.'
+                    )
+                }
+                stash(includes: '*-config', name: 'new_k8s_profiles')
+            }
+        }
+        stage('Encrypt') {
+            dir('digitallyinspired-todo-deployment/jenkins-users/sealed-configs') {
+                unstash('new_ovpn_profiles')
+                unstash('new_k8s_profiles')
                 files = findFiles(glob: '*.ovpn')
                 for (file in files) {
-                    def user = "${file.name}"
-                    user = user.take(user.lastIndexOf('.'))
-                    sh "kubectl create secret generic ${user}-ovpn-secret --dry-run --from-file=${file.name} -o yaml > ${user}.yaml"
-                    sh "kubeseal < ${user}.yaml > ${user}.yaml.sealed"
-                    stash(includes: '*.yaml.sealed', name: 'new_sealed_profiles')
+                    def file = "${file.name}"
+                    encryptViaKubeseal(file, file.take(file.lastIndexOf('.')), 'sealed.yaml')
                 }
+                files = findFiles(glob: '*-config')
+                for (file in files) {
+                    def file = "${file.name}"
+                    encryptViaKubeseal(file, file.take(file.lastIndexOf('.')), 'sealed.yaml')
+                }
+                stash(includes: '*.yaml.sealed', name: 'new_sealed_profiles')
             }
-	}
-        stage('Push') {
-            sh 'tree'
-            unstash('new_sealed_profiles')
-            files = findFiles(glob: '*.yaml.sealed')
-            for (file in files) {
-                sh "git add ${file.name}"
-            }
-            sh "git commit -m 'Generated sealed user profile'"
-            sh 'git push origin master'
         }
+//         stage('Push') {
+//         sh 'tree'
+//            unstash('new_sealed_profiles')
+//            files = findFiles(glob: '*.yaml.sealed')
+//            for (file in files) {
+//                sh "git add ${file.name}"
+//            }
+//            sh "git commit -m 'Generated sealed user profile'"
+//            sh 'git push origin master'
+//        }
     }
 }
+
+def encryptViaKubeseal(user, file, extension) {
+    sh "kubectl create secret generic ${user}-ovpn-secret --dry-run --from-file=${file.name} -o yaml > ${user}.yaml"
+    sh "kubeseal < ${user}.yaml > ${user}.${extension}"
+}
+
